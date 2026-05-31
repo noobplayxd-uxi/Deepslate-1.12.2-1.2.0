7831801e96b10337a3cf495b04fe13574f816b13
NoobplayzXD <asmitroy888@gmail.com>
2026-05-31 10:37:05 +0530

    WorldProviderMixin.java

--- src/main/java/com/yourname/deepslateexpansion/mixins/minecraft/world/WorldProviderMixin.java
+++ src/main/java/com/yourname/deepslateexpansion/mixins/minecraft/world/WorldProviderMixin.java
@@ -8,11 +8,13 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 @Mixin(WorldProvider.class)
 public abstract class WorldProviderMixin {
 
+    // Changes the constant 256 returned by getHeight() to 384
     @ModifyConstant(method = "getHeight", constant = @Constant(intValue = 256))
     private int modifyGetHeight(int original) {
         return 384;
     }
 
+    // getActualHeight() also uses 256 internally; we override it the same way
     @ModifyConstant(method = "getActualHeight", constant = @Constant(intValue = 256))
     private int modifyGetActualHeight(int original) {
         return 384;
