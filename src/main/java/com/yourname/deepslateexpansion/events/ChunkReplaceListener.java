2a2bf7b64933bd8f85a43e727d191da173c9964f
NoobplayzXD <asmitroy888@gmail.com>
2026-05-31 17:03:00 +0530

    ChunkReplaceListener.java

---
+++ src/main/java/com/yourname/deepslateexpansion/events/ChunkReplaceListener.java
@@ -0,0 +1,39 @@
+package com.yourname.deepslateexpansion.events;
+
+import com.yourname.deepslateexpansion.blocks.ModBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraftforge.event.world.ChunkDataEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod.EventBusSubscriber(modid = "deepslateexpansion")
+public class ChunkReplaceListener {
+
+    @SubscribeEvent
+    public static void onChunkLoad(ChunkDataEvent.Load event) {
+        Chunk chunk = event.getChunk();
+
+        // Replace nether bricks with deepslate
+        replaceWrongBlock(chunk, Blocks.NETHER_BRICK, ModBlocks.deepslate);
+
+        // TODO: Add ore replacements when you identify the wrong IDs they map to
+    }
+
+    private static void replaceWrongBlock(Chunk chunk, Block wrongBlock, Block correctBlock) {
+        int minY = -64;
+        int maxY = 319;
+        for (int x = 0; x < 16; x++) {
+            for (int z = 0; z < 16; z++) {
+                for (int y = minY; y <= maxY; y++) {
+                    BlockPos pos = new BlockPos(x, y, z);
+                    if (chunk.getBlockState(pos).getBlock() == wrongBlock) {
+                        chunk.setBlockState(pos, correctBlock.getDefaultState());
+                    }
+                }
+            }
+        }
+    }
+}
