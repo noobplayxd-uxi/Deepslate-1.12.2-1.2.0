880ce972c2178f762d683a173dd3ad801c2c77e9
NoobplayzXD <asmitroy888@gmail.com>
2026-05-31 09:48:42 +0530

    DeepslateExpansion.java

--- src/main/java/com/yourname/deepslateexpansion/DeepslateExpansion.java
+++ src/main/java/com/yourname/deepslateexpansion/DeepslateExpansion.java
@@ -1,26 +1,36 @@
 package com.yourname.deepslateexpansion;
 
-import com.yourname.deepslateexpansion.Blocks.ModBlocks;
+import com.yourname.deepslateexpansion.blocks.ModBlocks;
+import com.yourname.deepslateexpansion.proxy.CommonProxy;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 
 @Mod(modid = DeepslateExpansion.MODID, name = DeepslateExpansion.NAME, version = DeepslateExpansion.VERSION)
 public class DeepslateExpansion {
-
     public static final String MODID = "deepslateexpansion";
     public static final String NAME = "Deepslate Expansion";
     public static final String VERSION = "1.0.0";
 
-    @EventHandler
+    @SidedProxy(clientSide = "com.yourname.deepslateexpansion.proxy.ClientProxy",
+                serverSide = "com.yourname.deepslateexpansion.proxy.CommonProxy")
+    public static CommonProxy proxy;
+
+    @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        // This creates all our block instances.
         ModBlocks.init();
+        proxy.preInit(event);
     }
 
-    @EventHandler
+    @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-        System.out.println(NAME + " has loaded successfully!");
+        proxy.init(event);
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        proxy.postInit(event);
     }
 }
