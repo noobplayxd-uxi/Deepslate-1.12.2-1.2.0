6aa1c40a757189d2c20086fbf9117c84ec63c390
NoobplayzXD <asmitroy888@gmail.com>
2026-05-31 18:07:08 +0530

    ClientProxy.java

--- src/main/java/com/yourname/deepslateexpansion/proxy/ClientProxy.java
+++ src/main/java/com/yourname/deepslateexpansion/proxy/ClientProxy.java
@@ -1,6 +1,8 @@
 package com.yourname.deepslateexpansion.proxy;
 
 import com.yourname.deepslateexpansion.network.ExtendedChunkHandler;
+import com.yourname.deepslateexpansion.events.ChunkReplaceHandler;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -10,16 +12,13 @@ import net.minecraftforge.fml.relauncher.Side;
 
 public class ClientProxy extends CommonProxy {
 
-    // The network channel for receiving extended chunk data
     public static final SimpleNetworkWrapper NETWORK = NetworkRegistry.INSTANCE.newSimpleChannel("extchunk");
 
     @Override
     public void preInit(FMLPreInitializationEvent event) {
         super.preInit(event);
-
-        // Register our custom packet handler. This will receive the extended chunk data
-        // from the ViaBackwards proxy and load it into the client's chunk storage.
         NETWORK.registerMessage(ExtendedChunkHandler.class, ExtendedChunkHandler.class, 0, Side.CLIENT);
+        MinecraftForge.EVENT_BUS.register(new ChunkReplaceHandler());
     }
 
     @Override
