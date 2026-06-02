608319b0b807d1bc50f0cee285ba62821e16a08a
NoobplayzXD <asmitroy888@gmail.com>
2026-06-02 19:48:57 +0530

    ClientProxy.java

--- src/main/java/com/yourname/deepslateexpansion/proxy/ClientProxy.java
+++ src/main/java/com/yourname/deepslateexpansion/proxy/ClientProxy.java
@@ -1,28 +1,23 @@
 package com.yourname.deepslateexpansion.proxy;
 
-import com.yourname.deepslateexpansion.network.ExtendedChunkHandler;
 import com.yourname.deepslateexpansion.events.ChunkReplaceHandler;
 import com.yourname.deepslateexpansion.events.DeepslateReplacer;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.network.NetworkRegistry;
-import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
+@SideOnly(Side.CLIENT)
 public class ClientProxy extends CommonProxy {
-
-    public static final SimpleNetworkWrapper NETWORK = NetworkRegistry.INSTANCE.newSimpleChannel("extchunk");
 
     @Override
     public void preInit(FMLPreInitializationEvent event) {
         super.preInit(event);
-        // Register the extended chunk packet handler
-        NETWORK.registerMessage(ExtendedChunkHandler.class, ExtendedChunkHandler.class, 0, Side.CLIENT);
-        // Register the chunk load replacement handler (optional, harmless)
+        // Register the chunk‑load replacement handler (optional, harmless)
         MinecraftForge.EVENT_BUS.register(new ChunkReplaceHandler());
-        // Register the tick‑based replacement scanner – guaranteed to work
+        // Register the tick‑based replacement scanner (deepslate above Y=0)
         MinecraftForge.EVENT_BUS.register(new DeepslateReplacer());
     }
 
