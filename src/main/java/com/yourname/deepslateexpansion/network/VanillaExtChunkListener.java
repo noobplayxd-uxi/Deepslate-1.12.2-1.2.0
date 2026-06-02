2d38f6c33ec44c0fe6fce40f1768196caa163c4e
NoobplayzXD <asmitroy888@gmail.com>
2026-06-02 19:44:41 +0530

    VanillaExtChunkListener.java

---
+++ src/main/java/com/yourname/deepslateexpansion/network/VanillaExtChunkListener.java
@@ -0,0 +1,78 @@
+package com.yourname.deepslateexpansion.network;
+
+import com.yourname.deepslateexpansion.mixins.minecraft.chunk.IChunkExtended;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ChunkProviderClient;
+import net.minecraft.client.network.NetHandlerPlayClient;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.network.play.server.SPacketCustomPayload;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.network.FMLNetworkEvent.ClientCustomPacketEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
+public class VanillaExtChunkListener {
+
+    @SubscribeEvent
+    public void onCustomPacket(ClientCustomPacketEvent event) {
+        // Vanilla custom payload packet
+        if (event.getPacket() instanceof SPacketCustomPayload) {
+            SPacketCustomPayload packet = (SPacketCustomPayload) event.getPacket();
+            if ("extchunk".equals(packet.getChannelName())) {
+                handleExtendedChunk(packet.getBufferData());
+                event.setCanceled(true); // no other handler needs this
+            }
+        }
+    }
+
+    private void handleExtendedChunk(PacketBuffer buf) {
+        try {
+            int chunkX = buf.readInt();
+            int chunkZ = buf.readInt();
+            boolean groundUp = buf.readBoolean();
+            int sectionCount = buf.readVarInt();
+
+            int[] sectionY = new int[sectionCount];
+            byte[][] blockData = new byte[sectionCount][];
+            byte[][] blockLight = new byte[sectionCount][];
+            byte[][] skyLight = new byte[sectionCount][];
+
+            for (int i = 0; i < sectionCount; i++) {
+                sectionY[i] = buf.readInt();
+                int blockDataLen = buf.readVarInt();
+                blockData[i] = new byte[blockDataLen];
+                buf.readBytes(blockData[i]);
+
+                int lightLen = buf.readVarInt();
+                blockLight[i] = new byte[lightLen];
+                buf.readBytes(blockLight[i]);
+
+                lightLen = buf.readVarInt();
+                skyLight[i] = new byte[lightLen];
+                buf.readBytes(skyLight[i]);
+            }
+
+            Minecraft mc = Minecraft.getMinecraft();
+            if (mc.world == null) return;
+
+            ChunkProviderClient provider = mc.world.getChunkProvider();
+            Chunk chunk = provider.provideChunk(chunkX, chunkZ);
+            if (chunk == null) {
+                chunk = new Chunk(mc.world, chunkX, chunkZ);
+                provider.loadChunk(chunkX, chunkZ);
+            }
+
+            if (chunk instanceof IChunkExtended) {
+                ((IChunkExtended) chunk).loadExtendedSections(
+                    sectionY, blockData, blockLight, skyLight, groundUp);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
